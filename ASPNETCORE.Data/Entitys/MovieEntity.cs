4b4a06b64895feab6d515c34b0ca58ffea2e9994
Andre Delfs <tasmaniandev69@proton.me>
2023-06-26 16:59:30 +0200

    3 Layers + EF Scaffold in Presentationlayer

---
+++ ASPNETCORE.Data/Entitys/MovieEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCORE.Data.Entitys;
+public class MovieEntity
+{
+	public int Id { get; set; }
+	public string? Title { get; set; }
+	[DataType(DataType.Date)]
+	public DateTime ReleaseDate { get; set; }
+	public string? Genre { get; set; }
+	public decimal Price { get; set; }
+}
