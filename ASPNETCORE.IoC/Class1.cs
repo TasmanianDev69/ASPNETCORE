3c99fd309f6dd4d9f821a177d35bf4ace6243518
Andre Delfs <tasmaniandev69@proton.me>
2023-06-26 17:01:54 +0200

    IOC Project

---
+++ ASPNETCORE.IoC/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ASPNETCORE.IoC
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection ConfigureServices(this IServiceCollection serviceCollection)
+		{
+			return serviceCollection;
+		}
+	}
+}
